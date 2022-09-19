fc2ed49274fc6f84714f7f6927fec87b64227cf3
Coder-Nicki <nicki5389@gmail.com>
2022-09-19 13:18:25 +1000

    Added a while loop to game mode and practise mode

---
+++ README.md
@@ -0,0 +1,7 @@
+# timesTable Challenge
+
+## Overview
+
+## Features
+
+The first feature of this application is 'Game Mode'. Users start at level one and have to answer multiplication sums correctly in order to move up to the next level. However, if they get too many wrong, then they are out of the game. There are 10 levels that all increase in difficulty. Can you be named the champion and complete all levels successfully?
--- main.py
+++ main.py
@@ -4,33 +4,26 @@ import time
 
 # Main code block
 name = input('What is your name: ')
-print('')
 print(f'Welcome {name} to the timesTables Challenge!')
-print('')
-time.sleep(1)
-print('Please choose a mode:')
-print('')
-time.sleep(1)
-mode = input('- Game mode [press g]\n- Practice mode [press p]: ')
-print('')
-if mode == 'g' or mode == 'G':
-    print('')
-    print('GET READY')
-    time.sleep(3)
-    print('Let\'s start round one!')
-    time.sleep(3)
-    game_mode.round_one()
-else:
-    difficulty = input('What level do you want to practise?\n- Easy[e]\n- Medium[m]\n- Hard[h]\n')
-    print('')
-    print('Get ready!')
-    time.sleep(2)
-    print('')
-    if difficulty == 'e' or difficulty == 'E':
-        practice_mode.easy_practise()
-    elif difficulty == 'm' or difficulty == 'M':
-        practice_mode.medium_practise()
-    elif difficulty == 'h' or difficulty == 'H':
-        practice_mode.hard_practise()
-    # else: (Need to factor in user pressing something else)
-    #     print('Please type [e], [m] or [h] to choose your difficulty level.')
+while True:
+    mode = input('Please choose a mode: \n- Game mode [press g]\n- Practice mode [press p]: ')
+    if mode == 'g' or mode == 'G':
+        print('GET READY')
+        time.sleep(3)
+        print('Let\'s start round one!')
+        time.sleep(3)
+        game_mode.round_one()
+    elif mode == 'p' or mode == 'P':
+        while True:
+            difficulty = input('What level do you want to practise?\n- Easy[e]\n- Medium[m]\n- Hard[h]\n')
+            if difficulty == 'e' or difficulty == 'E':
+                practice_mode.easy_practise()
+            elif difficulty == 'm' or difficulty == 'M':
+                practice_mode.medium_practise()
+            elif difficulty == 'h' or difficulty == 'H':
+                practice_mode.hard_practise()
+            else:
+                print('Please type in a lowercase letter e, m or h')
+    else:
+        print(f"Please type a lowercase 'g' or 'p'")
+
