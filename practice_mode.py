e2b214c1b7ecbe5b2b0503c6fd579105af04e31a
Coder-Nicki <nicki5389@gmail.com>
2022-09-21 14:23:40 +1000

    made practice mode DRY by using a class and objects

--- practice_mode.py
+++ practice_mode.py
@@ -3,80 +3,47 @@ import time
 
 # Practice mode functions
 
-def easy_practise():
-    correct_count = 0
-    incorrect_count = 0
-    start_time = time.time()
-    while correct_count < 100:
-        num1 = randint(1, 7)
-        num2 = randint(1, 7)
-        answer = input(f'{num1} * {num2} = ')
-        if answer == 'q' or answer == 'Q':
-            duration = round(time.time() - start_time, 2)
-            if correct_count > 20 and incorrect_count < 3:
-                print(f'You are AMAZING! You got {correct_count} correct and made {incorrect_count} error/s in {duration} seconds!')
-                break
-            elif correct_count == 100:
-                print(f'You are a champion! You got {correct_count} correct and made {incorrect_count} error/s in {duration} seconds!!')
-                break
-            else:
-                print(f'You got {correct_count} correct and made {incorrect_count} error/s in {duration} seconds!')
-                break
-        elif num1 * num2 == int(answer):
-            correct_count += 1
-            continue
-        else:
-            incorrect_count += 1
-            continue
+class Practice:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-def medium_practise():
-    correct_count = 0
-    incorrect_count = 0
-    start_time = time.time()
-    while correct_count < 100:
-        num1 = randint(1, 12)
-        num2 = randint(1, 12)
-        answer = input(f'{num1} * {num2} = ')
-        if answer == 'q' or answer == 'Q':
-            duration = round(time.time() - start_time, 2)
-            if correct_count > 20 and incorrect_count < 3:
-                print(f'You are AMAZING! You got {correct_count} correct and made {incorrect_count} error/s in {duration} seconds!')
-                break
-            elif correct_count == 100:
-                print(f'You are a champion! You got {correct_count} correct and made {incorrect_count} error/s in {duration} seconds!')
-                break
-            else:
-                print(f'You got {correct_count} correct and made {incorrect_count} error/s in {duration} seconds!')
-                break
-        elif num1 * num2 == int(answer):
-            correct_count += 1
-            continue
-        else:
-            incorrect_count += 1
-            continue
 
-def hard_practise():
-    correct_count = 0
-    incorrect_count = 0
-    start_time = time.time()
-    while correct_count < 100:
-        num1 = randint(1, 100)
-        num2 = randint(1, 100)
-        answer = input(f'{num1} * {num2} = ')
-        if answer == 'q' or answer == 'Q':
-            duration = round(time.time() - start_time, 2)
-            if correct_count > 20 and incorrect_count < 3:
-                print(f'You are AMAZING! You got {correct_count} correct and made {incorrect_count} error/s in {duration} seconds!')
-                break
-            elif correct_count == 100:
-                print(f'You are an absolute times table genius! You got {correct_count} correct and made {incorrect_count} error/s in {duration} seconds!')
-                break
+    def practise_mode(self):
+        correct_count = 0
+        incorrect_count = 0
+        start_time = time.time()
+        while correct_count < 100:
+            num = self.get_numbers()
+            answer = input(f'{num[0]} * {num[1]} = ')
+            if answer == 'q' or answer == 'Q':
+                duration = round(time.time() - start_time, 2)
+                if correct_count > 20 and incorrect_count < 3:
+                    print(f'You are AMAZING! You got {correct_count} correct and made {incorrect_count} error/s in {duration} seconds!')
+                    break
+                elif correct_count == 100:
+                    print(f'You are a champion! You got {correct_count} correct and made {incorrect_count} error/s in {duration} seconds!!')
+                    break
+                else:
+                    print(f'You got {correct_count} correct and made {incorrect_count} error/s in {duration} seconds!')
+                    break
+            elif num[0] * num[1] == int(answer):
+                correct_count += 1
+                num = self.get_numbers()
+                continue
             else:
-                print(f'You got {correct_count} correct and made {incorrect_count} error/s in {duration} seconds!')
-                break
-        elif num1 * num2 == int(answer):
-            correct_count += 1
-            continue
-        else:
-            incorrect_count += 1
-            continue
+                incorrect_count += 1
+                num = self.get_numbers()
+                continue
+
+    def get_numbers(self):
+        num1 = randint(self.x, self.y)
+        num2 = randint(self.x, self.y)
+        return num1, num2 
+
+easy = Practice(1, 7)
+medium = Practice(1, 12)
+hard = Practice(1, 20)
+
+easy.practise_mode()
+
