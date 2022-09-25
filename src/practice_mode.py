4d263cffe1a1a1d381a9ab784b9fc3d4e1cd7f27
Coder-Nicki <nicki5389@gmail.com>
2022-09-25 20:45:55 +1000

    deleted unused code in practice mode

--- src/practice_mode.py
+++ src/practice_mode.py
@@ -2,9 +2,6 @@ from random import randint
 import time
 import clearing
 
-class RangeError(Exception):
-    pass
-
 # Practice mode functions
 
 class Practice:
@@ -19,8 +16,9 @@ class Practice:
         start_time = time.time()
         while correct_count < 100:
             num = self.get_numbers()
-            answer = input(f'{num[0]} * {num[1]} = ')
-            if answer == 'q' or answer == 'Q':
+            actual_answer = num[0] * num[1]
+            user_answer = input(f'{num[0]} * {num[1]} = ')
+            if user_answer == 'q' or user_answer == 'Q':
                 duration = round(time.time() - start_time, 2)
                 if correct_count > 20 and incorrect_count < 3:
                     print(f'You are AMAZING! You got {correct_count} correct and made {incorrect_count} error/s in {duration} seconds!')
@@ -31,7 +29,11 @@ class Practice:
                 else:
                     print(f'You got {correct_count} correct and made {incorrect_count} error/s in {duration} seconds!')
                     break
-            elif num[0] * num[1] == int(answer):
+            elif actual_answer == int(user_answer):
+                correct_count += 1
+                num = self.get_numbers()
+                continue
+            elif actual_answer == int(user_answer):
                 correct_count += 1
                 num = self.get_numbers()
                 continue
@@ -44,28 +46,3 @@ class Practice:
         num1 = randint(self.x, self.y)
         num2 = randint(self.x, self.y)
         return num1, num2 
-
-def practice_number():
-        correct = 0
-        incorrect = 0
-        number = int(input('What number would you like to practise multiplying by between 1 and 12: '))
-        if not number in range(1, 13):
-            raise RangeError(f'You entered {number}. You must enter a number between 1 and 12')
-        while correct < 50:
-            number_two = randint(1, 12)
-            try:
-                user_answer = input(f'{number} * {number_two} = ')
-            except RangeError:
-                print('Not in range')
-            if number * number_two == user_answer:
-                count += 1
-            elif user_answer == 'q':
-                print(f'You got {correct} correct and made {incorrect} errors!')
-                time.sleep(3)
-                clearing.clear()
-                return
-            else:
-                incorrect += 1
-
-
-
